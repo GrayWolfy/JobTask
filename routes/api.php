bedaffe01dcf3fdb5acd4a89ee427d7928e5124c
GrayWolfy <sergeypopov23@gmail.com>
2022-11-21 13:11:54 +0300

    Updated specification

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/auth/register', [AuthController::class, 'register'])->name('register');
+Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
 Route::get('/parse', [ParseController::class, 'parse'])->name('pass');
 Route::post('/message/create', [MessageController::class, 'create'])->name('message.create');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
 
     /** Car Rent Block */
     Route::get('/car/index', [CarController::class, 'index'])->name('index');
